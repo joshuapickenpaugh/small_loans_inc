a98cc2c29f94772aa8161b927617d2040fa96fc4
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-07 10:15:15 -0500

    First Commit

--- Small_Loans_Inc/My Project/Application.Designer.vb
+++ Small_Loans_Inc/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Small_Loans_Inc.Form1
+            Me.MainForm = Global.Small_Loans_Inc.frmMain
         End Sub
     End Class
 End Namespace
