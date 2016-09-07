c2dccc763dc176e1dcfe20ea503bbb4bde73c9ef
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-07 10:31:47 -0500

    Another commit

--- Small_Loans_Inc/Form1.Designer.vb
+++ Small_Loans_Inc/Form1.Designer.vb
@@ -22,18 +22,96 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSalary = New System.Windows.Forms.TextBox()
+        Me.chkFiveYearsEmployed = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(52, 11)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "&Salary:"
+        '
+        'txtSalary
+        '
+        Me.txtSalary.Location = New System.Drawing.Point(37, 39)
+        Me.txtSalary.Name = "txtSalary"
+        Me.txtSalary.Size = New System.Drawing.Size(120, 20)
+        Me.txtSalary.TabIndex = 2
+        '
+        'chkFiveYearsEmployed
+        '
+        Me.chkFiveYearsEmployed.AutoSize = True
+        Me.chkFiveYearsEmployed.Location = New System.Drawing.Point(40, 65)
+        Me.chkFiveYearsEmployed.Name = "chkFiveYearsEmployed"
+        Me.chkFiveYearsEmployed.Size = New System.Drawing.Size(114, 17)
+        Me.chkFiveYearsEmployed.TabIndex = 3
+        Me.chkFiveYearsEmployed.Text = "5 &years employed?"
+        Me.chkFiveYearsEmployed.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtSalary)
+        Me.GroupBox1.Controls.Add(Me.chkFiveYearsEmployed)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 67)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Applicant Info:"
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAnswer.Location = New System.Drawing.Point(12, 170)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(200, 67)
+        Me.lblAnswer.TabIndex = 5
+        Me.lblAnswer.Text = "Label2"
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(224, 247)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "frmMain"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Small Loans Inc."
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtSalary As TextBox
+    Friend WithEvents chkFiveYearsEmployed As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblAnswer As Label
 End Class
